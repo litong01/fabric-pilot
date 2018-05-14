2a0ddcee070714f5042d927ed0c6f15bbe0a9873
litong01 <litong01@users.noreply.github.com>
2018-05-14 08:32:01 -0400

    Update syncgit.sh

--- serverscripts/syncgit.sh
+++ serverscripts/syncgit.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 # Schedule the next run regardless
 cd /home/ibmadmin/gitserver/synch
-at now + 5 minute -f thesync.sh > next.log 2>&1
+at now + 5 minute -f syncher.sh > /dev/null 2>&1
 
-cd /home/ibmadmin/hl/src/hfrd
-if git diff-index --quiet HEAD --; then
-    # No changes
-    echo 'no changes found' >> /home/ibmadmin/gitserver/synch/next.log
-else
-    # We have some changes, rebuild the  api server docker image
-    git pull
-    make api-docker
-    docker restart hfrdserver
+cd /home/ibmadmin/gitserver/hfrd.git
+mycheck=$(git fetch 2>&1)
+if [ ! -z "$mycheck" ]; then
     # Sync to the mirror git server
     eval $(ssh-agent -s)
     ssh-add /home/ibmadmin/.ssh/fd
-    cd /home/ibmadmin/gitserver/hfrd.git
-    git fetch
     git push --mirror ssh://git@9.42.91.228:2222/git-server/repos/hfrd.git
     kill -9 $SSH_AGENT_PID
+    echo 'Changes synced at '$(date) >> /home/ibmadmin/gitserver/synch/syncher.log
+
+    # We have some changes, rebuild the  api server docker image
+    cd /home/ibmadmin/hl/src/hfrd
+    git pull
+    make api-docker
+    docker restart hfrdserver
 fi
