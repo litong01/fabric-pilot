fc57835e83df202cbb318da8a724ba73c002b04a
litong01 <litong01@users.noreply.github.com>
2018-05-16 11:51:00 -0400

    Update syncgit.sh

--- serverscripts/syncgit.sh
+++ serverscripts/syncgit.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 # Schedule the next run regardless
-cd /home/ibmadmin/gitserver/synch
-at now + 5 minute -f syncher.sh > /dev/null 2>&1
+cd ${HOME}/gitserver/synch
+at now + 5 minute -f syncgit.sh > /dev/null 2>&1
 
-cd /home/ibmadmin/gitserver/hfrd.git
+cd ${HOME}/gitserver/hfrd.git
 mycheck=$(git fetch 2>&1)
 if [ ! -z "$mycheck" ]; then
+    mycheck=$(echo $mycheck | grep 'fatal: unable to access')
+
+    if [ ! -z "$mycheck" ]; then
+        echo 'Connection problem at '$(date) >> ${HOME}/gitserver/synch/syncgit.log
+        exit
+    fi
+
     # Sync to the mirror git server
     eval $(ssh-agent -s)
-    ssh-add /home/ibmadmin/.ssh/fd
+    ssh-add ${HOME}/.ssh/fd
     git push --mirror ssh://git@9.42.91.228:2222/git-server/repos/hfrd.git
     kill -9 $SSH_AGENT_PID
-    echo 'Changes synced at '$(date) >> /home/ibmadmin/gitserver/synch/syncher.log
+    echo 'Changes synced on '$(date) >> ${HOME}/gitserver/synch/syncgit.log
 
     # We have some changes, rebuild the  api server docker image
-    cd /home/ibmadmin/hl/src/hfrd
+    cd ${HOME}/hl/src/hfrd
     git pull
     make api-docker
     docker restart hfrdserver
+
+    # Update the swagger server
+    cp api/swagger.yaml ${HOME}/api/var
+    docker run -v ${HOME}/api/var:/docs swaggerapi/swagger-codegen-cli generate \
+      -i /docs/swagger.yaml -l swagger -o /docs
+    docker restart swaggerserver
 fi
